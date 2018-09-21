2d05d24fbc5bb5d753f682acdb4cad2c6f5e7253
Dustin Prevatt <dustinprevatt@Dustins-MacBook-Pro.local>
2018-09-20 22:46:38 -0500

    Adding Counters to PCDisplay

--- src/app/pickup-counter-display/pickup-counter-display.component.ts
+++ src/app/pickup-counter-display/pickup-counter-display.component.ts
@@ -1,4 +1,7 @@
+
+import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'app-pickup-counter-display',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PickupCounterDisplayComponent implements OnInit {
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) { }
+
+  totalPickedUpCount: Observable<any>;
+  totalSalesCount: Observable<any>;
 
   ngOnInit() {
+    this.getPlatesToBePickedUp();
+    this.getTotalSalesCount();
+  }
+
+  getPlatesToBePickedUp() {
+    const totalPickedUpCounterRef = '/counters/PlatesPickedUp/count';
+    this.totalPickedUpCount = this.db.object<any>(totalPickedUpCounterRef).valueChanges();
+    return this.totalPickedUpCount;
+  }
+
+  getTotalSalesCount() {
+    const totalSalesCounterRef = '/counters/totalSales/count';
+    this.totalSalesCount = this.db.object<any>(totalSalesCounterRef).valueChanges();
+    return this.totalSalesCount;
   }
 
 }
