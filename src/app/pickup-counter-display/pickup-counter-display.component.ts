6fd9ce3bba95b505588ffb2a2c3d8bf573872f26
Dustin Prevatt <dustinprevatt@Dustins-MacBook-Pro.local>
2018-10-24 21:27:00 -0500

    added recount to pickup

--- src/app/pickup-counter-display/pickup-counter-display.component.ts
+++ src/app/pickup-counter-display/pickup-counter-display.component.ts
@@ -2,6 +2,7 @@
 import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFirestoreModule, AngularFirestoreCollection, AngularFirestoreDocument, AngularFirestore } from 'angularfire2/firestore';
 
 @Component({
   selector: 'app-pickup-counter-display',
@@ -10,12 +11,13 @@ import { AngularFireDatabase } from 'angularfire2/database';
 })
 export class PickupCounterDisplayComponent implements OnInit {
 
-  constructor(private db: AngularFireDatabase) { }
+  constructor(private db: AngularFireDatabase, private afs: AngularFirestore) { }
 
   totalPickedUpCount: Observable<any>;
   totalSalesCount: Observable<any>;
 
   ngOnInit() {
+    this.verifyTotalSalesPickeupCounter();
     this.getPlatesToBePickedUp();
     this.getTotalSalesCount();
   }
@@ -30,6 +32,29 @@ export class PickupCounterDisplayComponent implements OnInit {
     const totalSalesCounterRef = '/counters/totalSales/count';
     this.totalSalesCount = this.db.object<any>(totalSalesCounterRef).valueChanges();
     return this.totalSalesCount;
+  }
+
+
+  verifyTotalSalesPickeupCounter() {
+    const cadetSalesCol = this.afs.collection('CadetSales', refv => {
+      return refv.where('PlatePickedUp', '==', true);
+    });
+    cadetSalesCol.valueChanges().take(1).subscribe(b => {
+      // Set The counter
+      this.VerifyTotalSalesPickupCounter(b.length);
+    });
+  }
+
+  VerifyTotalSalesPickupCounter(count) {
+    const counterRef = this.db.database.ref('counters').child('totalSales');
+    counterRef.transaction(dv => {
+      if (!dv) {
+        return dv;
+      }
+      console.log('Setting total sales counter to ' + count);
+      dv.count = count;
+      return dv;
+    });
   }
 
 }
--- src/app/plate-pickup-page/plate-pickup-page.component.ts
+++ src/app/plate-pickup-page/plate-pickup-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFirestoreModule, AngularFirestoreCollection, AngularFirestoreDocument, AngularFirestore } from 'angularfire2/firestore';
 
 @Component({
   selector: 'app-plate-pickup-page',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlatePickupPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase, private afs: AngularFirestore) { }
 
   ngOnInit() {
+    this.verifyTotalSalesPickeupCounter();
+  }
+
+  verifyTotalSalesPickeupCounter() {
+    const cadetSalesCol = this.afs.collection('CadetSales', refv => {
+      return refv.where('PlatePickedUp', '==', true);
+    });
+    cadetSalesCol.valueChanges().take(1).subscribe(b => {
+      // Set The counter
+      this.VerifyTotalSalesPickupCounter(b.length);
+    });
+  }
+
+  VerifyTotalSalesPickupCounter(count) {
+    const counterRef = this.db.database.ref('counters').child('totalSales');
+    counterRef.transaction(dv => {
+      if (!dv) {
+        return dv;
+      }
+      console.log('Setting total sales counter to ' + count);
+      dv.count = count;
+      return dv;
+    });
   }
 
 }
