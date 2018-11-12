2df86a3f7ad727b7a3054a7bdc091f022bc65838
Dustin Prevatt <dustinprevatt@Dustins-MacBook-Pro.local>
2018-11-11 23:23:51 -0600

    modification to Bronze Awards

--- src/app/cadet-awards/bronze-awards/bronze-awards.component.ts
+++ src/app/cadet-awards/bronze-awards/bronze-awards.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { AngularFirestoreModule, AngularFirestoreCollection, AngularFirestoreDocument, AngularFirestore } from 'angularfire2/firestore';
 import { Subscription } from 'rxjs/Subscription';
+import { TableExport } from 'tableexport';
 
 
 @Component({
@@ -16,6 +17,7 @@ export class BronzeAwardsComponent implements OnInit, OnDestroy {
   bronzeSales: Array<any>;
   bronzeAward: string;
   awardsBronzeSubscription: Subscription;
+  tableData: any;
 
   ngOnInit() {
     this.getBronzeSales();
@@ -48,7 +50,33 @@ export class BronzeAwardsComponent implements OnInit, OnDestroy {
     this.awardsBronzeSubscription = this.afs.doc<any>('/Rpt_SalesAward/Awards').valueChanges().subscribe(aw => {
       this.bronzeAward = aw.BronzeAward;
     });
+  }
 
-}
+  tableExportInit() {
+    // tslint:disable-next-line:no-unused-expression
+      this.tableData =  new TableExport(jQuery('#mytable'), {
+      headers: true,                              // (Boolean), display table headers (th or td elements) in the <thead>, (default: true)
+      footers: true,                              // (Boolean), display table footers (th or td elements) in the <tfoot>, (default: false)
+      formats: ['xlsx'],            // (String[]), filetype(s) for the export, (default: ['xlsx', 'csv', 'txt'])
+      // tslint:disable-next-line:max-line-length
+      filename: 'Bronze-Awards',                             // (id, String), filename for the downloaded file, (default: 'id')
+      bootstrap: true,                           // (Boolean), style buttons using bootstrap, (default: true)
+      // tslint:disable-next-line:max-line-length
+      exportButtons: false,                        // (Boolean), automatically generate the built-in export buttons for each of the specified formats (default: true)                        // (top, bottom), position of the caption element relative to table, (default: 'bottom')
+      ignoreRows: null,                           // (Number, Number[]), row indices to exclude from the exported file(s) (default: null)
+      ignoreCols: null,                           // (Number, Number[]), column indices to exclude from the exported file(s) (default: null)
+      trimWhitespace: false
+    }).reset();
+  }
+
+  export() {
+      this.tableExportInit();
+      console.log('initialized..');
+      const exportData = this.tableData.getExportData()['mytable']['xlsx'];
+      console.log('exportData Recieved..');
+        //                   // data          // mime              // name              // extension
+        this.tableData.export2file(exportData.data, exportData.mimeType, exportData.filename, exportData.fileExtension);
+        console.log('exported..');
+    }
 
 }
